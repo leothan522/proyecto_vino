092c22214852ce4dab7550b35b870d24fe0e307d
Yonathan Castillo <leothan522@gmail.com>
2025-09-04 18:56:49 -0400

    Optimizando los Seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,13 +15,24 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         //Usuario Root
-        User::factory()->create([
+        $root = User::factory()->create([
             'name' => !empty(env('ROOT_NAME')) ? env('ROOT_NAME') : 'Administrador',
             'email' => !empty(env('ROOT_EMAIL')) ? env('ROOT_EMAIL') : 'admin@morros-devops.xyz',
             'email_verified_at' => now(),
             'password' => Hash::make(env('ROOT_PASSWORD') ? env('ROOT_PASSWORD') : 'admin1234'),
             'is_root' => 1,
         ]);
+
+        //Usuario Administrador
+        if ($root->email != 'admin@morros-devops.xyz') {
+            $admin = User::factory()->create([
+                'name' => 'Administrador',
+                'email' => 'admin@morros-devops.xyz',
+                'email_verified_at' => now(),
+                'password' => Hash::make('admin1234'),
+            ]);
+            $admin->assignRole('admin');
+        }
 
     }
 }
