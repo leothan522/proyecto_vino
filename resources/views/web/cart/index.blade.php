d7846daa511d03128f18bcb3f0a16a52acc3082a
Yonathan Castillo <leothan522@gmail.com>
2025-09-14 02:34:10 -0400

    Cambios Menores

--- resources/views/web/cart/index.blade.php
+++ resources/views/web/cart/index.blade.php
@@ -6,6 +6,23 @@
 
     @include('web.layouts.hero-wrap-2')
 
-    @include('web.section.cart-section')
+    <section class="ftco-section">
+        @include('web.section.cart-section')
+    </section>
 
+@endsection
+
+@section('css')
+    <style>
+        input[type=number]::-webkit-inner-spin-button,
+        input[type=number]::-webkit-outer-spin-button {
+            -webkit-appearance: none;
+            margin: 0;
+        }
+
+        /* Para Firefox */
+        input[type=number] {
+            -moz-appearance: textfield;
+        }
+    </style>
 @endsection
