0c65c599d85ae9facfa32210bc5710624c444f55
Yonathan Castillo <leothan522@gmail.com>
2025-09-18 19:29:47 -0400

    Nuevo Carrito para Vista Movil

--- resources/views/livewire/web/cart-component.blade.php
+++ resources/views/livewire/web/cart-component.blade.php
@@ -6,7 +6,7 @@
 
         <div class="row @if($ftco_animate) ftco-animate @endif">
             <div class="table-responsive">
-                <table class="table">
+                <table class="table d-none d-md-table">
                     <thead class="thead-primary">
                     <tr>
                         <th>&nbsp;</th>
@@ -36,7 +36,7 @@
                             <td>${{ formatoMillares($item->producto->precio) }}</td>
                             <td class="quantity">
                                 <div class="input-group">
-                                    <input type="number" name="quantity" id="input_cantidad_item_{{ $item->id }}"
+                                    <input type="number" id="input_cantidad_item_{{ $item->id }}"
                                            class="quantity form-control input-number @if($this->isInvalidStock($item->almacenes_id, $item->productos_id, $item->cantidad)) is-invalid @endif"
                                            value="{{ $item->cantidad }}"
                                            @change="cargando = true; setTimeout(() => cargando = false, 2000); if($event.target.value > 0){ Livewire.dispatch('setCantidad', { item_id: {{ $item->id }}, cantidad: $event.target.value }); }else{ $event.target.value = '{{ $item->cantidad }}' }"/>
@@ -45,7 +45,8 @@
                             </td>
                             <td>${{ formatoMillares($item->cantidad * $item->producto->precio) }}</td>
                             <td>
-                                <button type="button" wire:click="removeCart({{ $item->productos_id }})" class="close" data-dismiss="alert" aria-label="Close">
+                                <button type="button" wire:click="removeCart({{ $item->productos_id }})" class="close"
+                                        data-dismiss="alert" aria-label="Close">
                                     <span aria-hidden="true"><i class="fa fa-close"></i></span>
                                 </button>
                             </td>
@@ -53,12 +54,69 @@
                     @endforeach
 
                     <!-- Spinner overlay -->
-                    <div wire:loading wire:target="checkOut, removeCart" class="spinner-overlay align-content-center text-center">
+                    <div wire:loading wire:target="checkOut, removeCart"
+                         class="spinner-overlay align-content-center text-center">
                         <div class="spinner-border color-active" role="status"></div>
                     </div>
 
                     </tbody>
                 </table>
+            </div>
+        </div>
+
+        <div class="row @if($ftco_animate) ftco-animate @endif d-md-none">
+            <div class="col-12 list-group">
+                @foreach($items as $item)
+                    <div class="list-group-item d-flex align-items-start alert" role="alert" href="#">
+                        <div class="p-2">
+                            <div class="img"
+                                 style="background-image: url({{ verImagen($item->producto->imagen_path) }}); min-width: 80px !important; min-height: 80px !important;">
+                                &nbsp;
+                            </div>
+                        </div>
+                        <div class="text pl-3">
+                            <button type="button" wire:click="removeCart({{ $item->productos_id }})" class="close"
+                                    data-dismiss="alert" aria-label="Close">
+                                <span class="text-muted" aria-hidden="true"><i class="fa fa-close"></i></span>
+                            </button>
+                            <h4>{{ $item->producto->nombre }}</h4>
+                            <small>{{ $item->almacen->nombre }}</small>
+                            <p class="mb-2">
+                                <a href="{{ route('web.single', $item->productos_id) }}"
+                                   class="price">${{ formatoMillares($item->producto->precio) }}</a>
+                                <span
+                                    class="quantity ml-3">Disponible: {{ $this->getDisponibles($item->almacenes_id, $item->productos_id) }}</span>
+                            </p>
+
+
+                            <div class="col-12 product-details p-0 mb-2">
+
+                                {{--Botones Cantidad--}}
+                                <div class="row">
+                                    <div class="input-group col-md-6 d-flex mb-3 position-relative">
+
+                                        {{-- Boton Menos--}}
+                                        <span class="input-group-btn mr-2 pt-1">
+                                            Cantidad:
+                                        </span>
+
+                                        {{--Input Cantidad--}}
+                                        <input type="number"
+                                               class="quantity form-control input-number @if($this->isInvalidStock($item->almacenes_id, $item->productos_id, $item->cantidad)) is-invalid @endif"
+                                               value="{{ $item->cantidad }}"
+                                               @change="cargando = true; setTimeout(() => cargando = false, 2000); if($event.target.value > 0){ Livewire.dispatch('setCantidad', { item_id: {{ $item->id }}, cantidad: $event.target.value }); }else{ $event.target.value = '{{ $item->cantidad }}' }"/>
+
+                                        <small class="invalid-tooltip">Excedido</small>
+
+                                    </div>
+                                </div>
+
+                            </div>
+
+
+                        </div>
+                    </div>
+                @endforeach
             </div>
         </div>
 
@@ -70,7 +128,9 @@
                     @include('web.section.totales-carrito')
                 </div>
                 <p class="text-center">
-                    <button id="boton_procesar_pedido" wire:click="checkOut" wire:loading.attr="disabled" wire:target="removeCart, checkOut" type="button" class="btn btn-primary py-3 px-4" :disabled="cargando" >
+                    <button id="boton_procesar_pedido" wire:click="checkOut" wire:loading.attr="disabled"
+                            wire:target="removeCart, checkOut" type="button" class="btn btn-primary py-3 px-4"
+                            :disabled="cargando">
                         Proceder al Pago
                     </button>
                 </p>
@@ -79,6 +139,8 @@
 
     @endif
 
-    <button id="buttonModalLoginFast_0" type="button" class="d-none" data-toggle="modal" data-target="#modalLoginFast">Modal Login Fast</button>
+    <button id="buttonModalLoginFast_0" type="button" class="d-none" data-toggle="modal" data-target="#modalLoginFast">
+        Modal Login Fast
+    </button>
 
 </div>
--- resources/views/web/section/totales-carrito.blade.php
+++ resources/views/web/section/totales-carrito.blade.php
@@ -1,13 +1,13 @@
 <p class="d-flex">
     <span>Subtotal</span>
-    <span>{{ formatoMillares($subtotal) }}</span>
+    <span class="text-right">${{ formatoMillares($subtotal) }}</span>
 </p>
 <p class="d-flex">
     <span>Entrega</span>
-    <span>${{ formatoMillares($entrega) }}</span>
+    <span class="text-right">${{ formatoMillares($entrega) }}</span>
 </p>
 <hr>
 <p class="d-flex total-price">
     <span>Total</span>
-    <span>${{ formatoMillares($total) }}</span>
+    <span class="text-right">BCV ${{ formatoMillares($total) }}</span>
 </p>
