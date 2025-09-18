406a2bf0fcb3ecc351bdfec64e2a0d65defc36b5
Yonathan Castillo <leothan522@gmail.com>
2025-09-18 08:41:57 -0400

    Cambios Menores

--- resources/views/web/section/list-products.blade.php
+++ resources/views/web/section/list-products.blade.php
@@ -9,22 +9,28 @@
                      style="background-image: url({{ verImagen($producto->imagen_path) }});">
                     <div class="desc">
                         <p class="meta-prod d-flex">
-                            <a href="#" wire:click.prevent="productAddCart({{ $producto->id }})" class="d-flex align-items-center justify-content-center">
+                            <a href="#" wire:click.prevent="productAddCart({{ $producto->id }})"
+                               class="d-flex align-items-center justify-content-center">
                                 @if($this->productInCart($producto->id))
                                     <span class="fa fa-shopping-bag text-warning"></span>
                                 @else
                                     <span class="flaticon-shopping-bag"></span>
                                 @endif
                             </a>
-                            <a href="#" wire:click.prevent="productAddFavorite({{ $producto->id }})" class="d-flex align-items-center justify-content-center">
+                            <a href="#" wire:click.prevent="productAddFavorite({{ $producto->id }})"
+                               class="d-flex align-items-center justify-content-center">
                                 @if($this->productIsFavorite($producto->id))
                                     <span class="fa fa-heart text-danger"></span>
                                 @else
                                     <span class="fa fa-heart-o"></span>
                                 @endif
                             </a>
-                            <button id="buttonModalLoginFast_{{ $producto->id }}" type="button" class="d-none" data-toggle="modal" data-target="#modalLoginFast">Modal Login Fast</button>
-                            <a href="#" wire:click.prevent="productShow({{ $producto->id }})" class="d-flex align-items-center justify-content-center"><span class="flaticon-visibility"></span></a>
+                            <button id="buttonModalLoginFast_{{ $producto->id }}" type="button" class="d-none"
+                                    data-toggle="modal" data-target="#modalLoginFast">Modal Login Fast
+                            </button>
+                            <a href="#" wire:click.prevent="productShow({{ $producto->id }})"
+                               class="d-flex align-items-center justify-content-center"><span
+                                    class="flaticon-visibility"></span></a>
                         </p>
                     </div>
                 </div>
@@ -36,19 +42,10 @@
                     <h2>{{ $producto->nombre }}</h2>
                     <span class="price">BCV ${{ formatoMillares($producto->precio) }}</span>
                 </div>
-                <!-- Spinner overlay -->
-                <div wire:loading wire:target="productShow({{ $producto->id }})" class="spinner-overlay align-content-center text-center">
-                    <div class="spinner-border color-active" role="status"></div>
-                </div>
-                <div wire:loading wire:target="productAddFavorite({{ $producto->id }})" class="spinner-overlay align-content-center text-center">
-                    <div class="spinner-border color-active" role="status"></div>
-                </div>
-                <div wire:loading wire:target="productAddCart({{ $producto->id }})" class="spinner-overlay align-content-center text-center">
-                    <div class="spinner-border color-active" role="status"></div>
-                </div>
+
             </div>
         </div>
-
+        
     @endforeach
 
 </div>
