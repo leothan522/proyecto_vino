63689540c22b4cb2d0b255b85f23179dca0c8ae9
Yonathan Castillo <leothan522@gmail.com>
2025-09-24 06:51:34 -0400

    Spinner en nabvar

--- resources/views/web/layouts/navbar.blade.php
+++ resources/views/web/layouts/navbar.blade.php
@@ -1,26 +1,82 @@
-<nav id="ftco-navbar" class="navbar navbar-expand-lg navbar-dark bg-dark ftco-navbar-light <!--ftco_navbar--> scrolled awake" >
-    <div class="container">
-        <a class="navbar-brand" href="{{ route('web.index') }}">
+<nav id="ftco-navbar"
+     class="navbar navbar-expand-lg navbar-dark bg-dark ftco-navbar-light <!--ftco_navbar--> scrolled awake">
+    <div x-data="{ activo: null }" class="container">
+
+        <a class="navbar-brand" href="{{ route('web.index') }}"  @click="activo = 1">
             <small>Vino <span>Don Juan Espinoza</span></small>
         </a>
 
-        <livewire:web.order-last-component />
+        <livewire:web.order-last-component/>
 
-        <button class="navbar-toggler mr-3" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler mr-3" type="button" data-toggle="collapse" data-target="#ftco-nav"
+                aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
             {{--<span class="oi oi-menu"></span> Menu--}}
             <i class="fa fa-bars" aria-hidden="true"></i>
         </button>
 
         <div class="collapse navbar-collapse" id="ftco-nav">
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item @if(Route::currentRouteName() == 'web.index') active @endif"><a href="{{ route('web.index') }}" class="nav-link">Inicio</a></li>
-                <li class="nav-item @if(Route::currentRouteName() == 'web.about') active @endif"><a href="{{ route('web.about') }}" class="nav-link">Acerca de</a></li>
-                <li class="nav-item @if(Route::currentRouteName() == 'web.products') active @endif"><a href="{{ route('web.products') }}" class="nav-link">Productos</a></li>
-                <li class="nav-item @if(Route::currentRouteName() == 'web.home') active @endif"><a href="{{ route('web.home') }}" class="nav-link">Mi Cuenta</a></li>
-                <li class="nav-item @if(Route::currentRouteName() == 'web.blog') active @endif"><a href="{{ route('web.blog') }}" class="nav-link">Galería</a></li>
-                <li class="nav-item @if(Route::currentRouteName() == 'web.contact') active @endif"><a href="{{ route('web.contact') }}" class="nav-link">Contacto</a></li>
-                <li class="nav-item d-md-none"><a href="{{ route('filament.dashboard.pages.dashboard') }}" class="nav-link">{{ __('Dashboard') }}</a></li>
+                <li class="nav-item @if(Route::currentRouteName() == 'web.index') active @endif">
+                    <a href="{{ route('web.index') }}"
+                       @click="activo = 1"
+                       :class="activo === 1 ? 'active' : ''"
+                       class="nav-link">
+                        Inicio
+                    </a>
+                </li>
+                <li class="nav-item @if(Route::currentRouteName() == 'web.about') active @endif">
+                    <a href="{{ route('web.about') }}"
+                       @click="activo = 2"
+                       :class="activo === 2 ? 'active' : ''"
+                       class="nav-link">
+                        Acerca de
+                    </a>
+                </li>
+                <li class="nav-item @if(Route::currentRouteName() == 'web.products') active @endif">
+                    <a href="{{ route('web.products') }}"
+                       @click="activo = 3"
+                       :class="activo === 3 ? 'active' : ''"
+                       class="nav-link">
+                        Productos
+                    </a>
+                </li>
+                <li class="nav-item @if(Route::currentRouteName() == 'web.home') active @endif">
+                    <a href="{{ route('web.home') }}"
+                       @click="activo = 4"
+                       :class="activo === 4 ? 'active' : ''"
+                       class="nav-link">
+                        Mi Cuenta
+                    </a>
+                </li>
+                <li class="nav-item @if(Route::currentRouteName() == 'web.blog') active @endif">
+                    <a href="{{ route('web.blog') }}"
+                       @click="activo = 5"
+                       :class="activo === 5 ? 'active' : ''"
+                       class="nav-link">
+                        Galería
+                    </a>
+                </li>
+                <li class="nav-item @if(Route::currentRouteName() == 'web.contact') active @endif">
+                    <a href="{{ route('web.contact') }}"
+                       @click="activo = 6"
+                       :class="activo === 6 ? 'active' : ''"
+                       class="nav-link">
+                        Contacto
+                    </a>
+                </li>
+                <li class="nav-item d-md-none">
+                    <a href="{{ route('filament.dashboard.pages.dashboard') }}"
+                       @click="activo = 7"
+                       :class="activo === 7 ? 'active' : ''"
+                       class="nav-link">
+                        {{ __('Dashboard') }}
+                    </a>
+                </li>
             </ul>
+        </div>
+
+        <div x-show="activo" class="spinner-overlay align-content-center d-md-none">
+            <div class="spinner-border color-active" role="status"></div>
         </div>
 
     </div>
