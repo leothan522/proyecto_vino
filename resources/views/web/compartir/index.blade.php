7377166d4f660aed0ea7c90df2dfa5c3d810d4c7
Yonathan Castillo <leothan522@gmail.com>
2025-09-25 10:54:06 -0400

    Cambios menores

--- resources/views/web/compartir/index.blade.php
+++ resources/views/web/compartir/index.blade.php
@@ -11,21 +11,46 @@
                     <div class="wrapper px-md-4">
                         <div class="row no-gutters justify-content-center">
 
-                            <div class="col-md-7">
-                                <div class="contact-wrap w-100 p-md-5 p-4">
+                            <div class="col-md-7 mb-4">
+                                <div class="contact-wrap w-100 p-4">
                                     <h3 class="mb-4">Descargar App</h3>
                                     <form method="POST" id="contactForm" name="contactForm" class="contactForm">
                                         <div class="row">
-                                            <div class="col-md-6 justify-content-center text-center mb-5">
-                                                <a href="{{ route('web.descargar') }}" class="label" for="name">Android</a>
+                                            <div x-data="{ cargando: false }" class="col-md-6 justify-content-center text-center mb-3 mb-md-auto">
+                                                <label class="label">Android</label>
                                                 <img class="img-fluid" src="{{ $qrAndroid }}" alt="Codigo QR" />
+                                                <a @click="cargando = true; setTimeout(() => cargando = false, 3000);" href="{{ route('web.descargar') }}" class="label" style="text-decoration: underline;">
+                                                    <i class="fa fa-cloud-download" aria-hidden="true"></i> Descargar
+                                                </a>
+                                                <div x-show="cargando" class="spinner-overlay align-content-center">
+                                                    <div class="spinner-border color-active" role="status"></div>
+                                                </div>
                                             </div>
-                                            <div class="col-md-6 justify-content-center text-center">
-                                                <a href="{{ route('web.index') }}" class="label" for="name">IOS</a>
+                                            <div x-data="{ cargando: false }" class="col-md-6 justify-content-center text-center mt-5 mt-md-auto">
+                                                <label class="label">IOS</label>
                                                 <img class="img-fluid" src="{{ $qrIos }}" alt="Codigo QR" />
+                                                <a @click="cargando = true" href="{{ route('web.index') }}" class="label" style="text-decoration: underline;">
+                                                    <i class="fa fa-external-link-square" aria-hidden="true"></i> Abrir
+                                                </a>
+                                                <div x-show="cargando" class="spinner-overlay align-content-center">
+                                                    <div class="spinner-border color-active" role="status"></div>
+                                                </div>
                                             </div>
                                         </div>
                                     </form>
+                                </div>
+                            </div>
+
+                            <div class="col-md-6">
+                                <div class="dbox w-100">
+                                    <div class="text text-center">
+                                        <p><span class="color-active">Instalación directa en iPhone desde Safari:</span></p>
+                                    </div>
+                                    <div class="text">
+                                        <p class="font-weight-bold">1. Abre el enlace del codigo QR para <span class="color-active">IOS</span> en Safari.</p>
+                                        <p class="font-weight-bold">2. - Toca “Compartir” → “Agregar a pantalla de inicio”.</p>
+                                        <p class="font-weight-bold">3. - Se instala la app y listo.</p>
+                                    </div>
                                 </div>
                             </div>
 
